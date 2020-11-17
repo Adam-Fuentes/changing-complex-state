b7cbecc01774d30dd92343c35499840452bf6a67
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-17 10:40:04 +0100

    Add logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function hendleChange(event) {
+    const {value, name} = event.target;
+
+    setFullName(prevValue =>{
+      if(name === "fName"){
+        return{
+          fName: value,
+          lName: prevValue.lName
+        }
+      }else if(name === "lName"){
+        return{
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" onChange={hendleChange} placeholder="First Name" value={fullName.fName} />
+        <input name="lName" onChange={hendleChange} placeholder="Last Name" value={fullName.lName} />
         <button>Submit</button>
       </form>
     </div>
